1594a457f0a4920343837e8232b0775458bc7d6e
Eduardo Vizcaino <evigra@hotmail.com>
2021-04-11 02:18:27 -0500

    Update

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
 		    if(isset($_COOKIE['SolesGPS']) AND !isset($_SESSION["var"]["action"]))
 			    $sys_location	="Location:sesion/&cookie=1";
 		    elseif(isset($_SESSION["var"]["server_true"]) AND in_array($_SERVER["SERVER_NAME"],@$_SESSION["var"]["server_true"]))	
-			    $sys_location	="Location:session/";							
+			    $sys_location	="Location:sesion/";							
 		    else													        						
 			    $sys_location	="Location:sesion/";
 			    
