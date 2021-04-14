15802c6688716e02de9f30129e07b023fd08237c
Eduardo Vizcaino <evigra@hotmail.com>
2021-04-14 06:22:15 -0500

    Update

--- modulos/users/modelo.php
+++ modulos/users/modelo.php
@@ -310,11 +310,11 @@
 		}	
    		public function __ACCION_VALIDAR()
     	{
-    	    $this->__PRINT($this->request["users"]);
-    	    /*
+    	    
+    	    
 			if(isset($this->request["users"]))
 			{
-			    
+			    $this->__PRINT_R($this->request["users"]);    
 		
 
 				foreach($this->request["users"] as $id)
@@ -327,7 +327,7 @@
 				}
 
 			} 
-			*/   	
+			   	
 		}	
    		public function __REPORT_NOVALIDO()    // PASO 1
     	{
