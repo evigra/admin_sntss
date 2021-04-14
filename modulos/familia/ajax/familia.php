be62205521347433161a493a8252e1d5162afb53
Eduardo Vizcaino <evigra@hotmail.com>
2021-04-14 16:24:34 -0500

    Update

--- modulos/familia/ajax/familia.php
+++ modulos/familia/ajax/familia.php
@@ -3,9 +3,6 @@
 	$option				=array();	
 	$option["name"]		="rh_c";	
 
-
-
-	
 	$objeto				=new familia($option);	
 	#$objeto->sys_private["section"]=="section_filtro";
 	#$objeto->sys_private["action"]=="section_filtro";
@@ -23,10 +20,9 @@
 		"user_id='{$_GET["id"]}'",
 	);
 
-
-
     $option["template_title"]	=$objeto->sys_var["module_path"]."html/ajax/report_title";
     $option["template_body"]	=$objeto->sys_var["module_path"]."html/ajax/report_body";			
+    $option["height"]="200px";
 					
 	$data										= $objeto->__VIEW_REPORT($option);
 	
